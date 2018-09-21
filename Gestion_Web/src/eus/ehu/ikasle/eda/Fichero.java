bfbd4ffb933fca12a93743a6e096363812231999
Daniel <origamix_014@hotmail.com>
2018-09-21 23:18:07 +0200

    pruebas

--- Gestion_Web/src/eus/ehu/ikasle/eda/Fichero.java
+++ Gestion_Web/src/eus/ehu/ikasle/eda/Fichero.java
@@ -1,5 +1,11 @@
 package eus.ehu.ikasle.eda;
 
+import eus.ehu.ikasle.eda.utils.Stopwatch;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 public class Fichero {
     private static Fichero ourInstance = new Fichero();
 
@@ -15,7 +21,20 @@ public class Fichero {
     }
 
     public void cargarWebs(){
+        HashMap<Integer,Web> webs = new HashMap<>();
 
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(System.getProperty("user.dir") + File.separator + "smallindex"));
+            String line;
+            //StringTokenizer stk = new StringTokenizer(line, " ");
+            while ((line = in.readLine()) != null){
+                webs.put(Integer.parseInt(line.substring(line.lastIndexOf(" ") + 1)),new Web(line.substring(0, line.indexOf(" "))));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void cargarRelaciones(){
--- Gestion_Web/src/eus/ehu/ikasle/eda/Main.java
+++ Gestion_Web/src/eus/ehu/ikasle/eda/Main.java
@@ -1,9 +1,13 @@
 package eus.ehu.ikasle.eda;
 
+import eus.ehu.ikasle.eda.utils.Stopwatch;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test    ");
+        Stopwatch stopwatch = new Stopwatch();
+        Fichero.getInstance().cargarWebs();
+        System.out.println(stopwatch.elapsedTime());
     }
 
 }
