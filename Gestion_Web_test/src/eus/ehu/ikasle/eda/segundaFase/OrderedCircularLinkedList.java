5a04f2a9bcd1626c23ab813e3e2c1213dca9b350
ander <anderct105@gmail.com>
2018-10-20 13:48:06 +0200

    Merge branch 'master' of https://gitlab.com/proyectos_uni/practica-1
    # Conflicts:
    #	Gestion_Web_test/src/eus/ehu/ikasle/eda/Webs.java

---
+++ Gestion_Web_test/src/eus/ehu/ikasle/eda/segundaFase/OrderedCircularLinkedList.java
@@ -0,0 +1,16 @@
+package eus.ehu.ikasle.eda.segundaFase;
+
+public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T> {
+	
+	public void add(T elem){
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+
+
+
+
+
+
+	}
+
+
+}
