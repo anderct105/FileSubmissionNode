5741c29fb6b71401c5d7b5d84d9eb8f98d960240
kaecius <origamix_014@hotmail.com>
2018-10-30 20:04:55 +0100

    pruebas unordered

--- Gestion_Web_test/src/eus/ehu/ikasle/eda/segundaFase/Main.java
+++ Gestion_Web_test/src/eus/ehu/ikasle/eda/segundaFase/Main.java
@@ -13,7 +13,12 @@ public class Main {
     }
 
     private static void testOrderedCircularLinkedList() {
-        //TODO mirar si realmente está bien puesto el enlace con los demas nodos
+
+    }
+
+
+
+    private static void testUnorderedCircularLinkedList() {
         UnorderedCircularLinkedList<Integer> basicTypeList = new UnorderedCircularLinkedList<Integer>();
         System.out.println("Testing integers UnorderedCircularLinkedList:");
         System.out.println("\t| addToRear test O(1):");
@@ -76,23 +81,88 @@ public class Main {
 
         //2 to < 1 >;
         System.out.print("\t\t-Adding 2 after last element (1) to 2 elements list: ");
-        basicTypeList.addToFront(2);
+        basicTypeList.addAfter(2,1);
         System.out.println(basicTypeList.contains(2) &&
                 basicTypeList.size() == 3);
         System.out.println("\t\t"+basicTypeList.toString());
         //7 to < 1 2 >
-        System.out.print("\t\t-Adding 7 to 2 elements list < 1 2 > :");
-        basicTypeList.addToFront(   7);
+        System.out.print("\t\t-Adding 7 between <0 1 2 > :");
+        basicTypeList.addAfter(   7,1);
         System.out.println(basicTypeList.contains(7)
-                && basicTypeList.size() == 3);
+                && basicTypeList.size() == 4);
         System.out.println("\t\t"+basicTypeList.toString());
-
-
+        //------------------------------------------------------
+        System.out.println();
         UnorderedCircularLinkedList<Persona> objectsList = new UnorderedCircularLinkedList<>();
+        System.out.println("Testing objects UnorderedCircularLinkedList:");
+        System.out.println("\t| addToRear test O(1):");
+        //addToRear
+        //1 to <>
+        System.out.println("\t\t"+objectsList.toString());
+        System.out.print("\t\t-Adding [pedro, 1111]: ");
+        objectsList.addToRear(new Persona("pedro","1111"));
+        System.out.println(objectsList.contains(new Persona("pedro","1111")));
+        System.out.println("\t\t"+objectsList.toString());
+        //2 to < 1 >;
+        System.out.print("\t\t-Adding [amaia , 2222]: ");
+        objectsList.addToRear(new Persona("amaia","2222"));
+        System.out.println(objectsList.contains(new Persona("amaia","2222")) &&
+                objectsList.size() == 2);
+        System.out.println("\t\t"+objectsList.toString());
+        //7 to < 1 2 >
+        System.out.print("\t\t-Adding [ana 3333]: ");
+        objectsList.addToRear(new Persona("ana","3333"));
+        System.out.println(objectsList.contains(new Persona("ana","3333"))
+                && objectsList.size() == 3);
+        System.out.println("\t\t"+objectsList.toString());
 
-    }
+        //addToFront
+        objectsList = new UnorderedCircularLinkedList<>();
+        //1 to <>
+        System.out.println("\t| addToFront test O(1)");
+        System.out.println("\t\t"+objectsList.toString());
+        System.out.print("\t\t-Adding [pedro, 1111]: ");
+        objectsList.addToFront(new Persona("pedro","1111"));
+        System.out.println(objectsList.contains(new Persona("pedro","1111")));
+        System.out.println("\t\t"+objectsList.toString());
+        //2 to < 1 >;
+        System.out.print("\t\t-Adding [amaia , 2222]: ");
+        objectsList.addToFront(new Persona("amaia","2222"));
+        System.out.println(objectsList.contains(new Persona("amaia","2222")) &&
+                objectsList.size() == 2);
+        System.out.println("\t\t"+objectsList.toString());
+        //7 to < 1 2 >
+        System.out.print("\t\t-Adding [ana 3333]: ");
+        objectsList.addToFront(new Persona("ana","3333"));
+        System.out.println(objectsList.contains(new Persona("ana","3333"))
+                && objectsList.size() == 3);
+        System.out.println("\t\t"+objectsList.toString());
 
-    private static void testUnorderedCircularLinkedList() {
+        //addAfter
+        objectsList = new UnorderedCircularLinkedList<>();
+        //1 to <>
+        System.out.println("\t| addAfter test O(n)");
+        System.out.println("\t\tPre: Target se encuentra en la lista");
+        objectsList.addToFront(new Persona("juan","0000"));
+        System.out.println("\t\t"+objectsList.toString());
+        System.out.print("\t\t-Adding [pedro, 1111]: ");
+        objectsList.addAfter(new Persona("pedro","1111"),new Persona("juan","0000"));
+        System.out.println(objectsList.contains(new Persona("pedro","1111")));
+        System.out.println("\t\t"+objectsList.toString());
+        //2 to < 1 >;
+        System.out.print("\t\t-Adding [amaia , 2222]: ");
+        objectsList.addAfter(new Persona("amaia","2222"),new Persona("pedro","1111"));
+        System.out.println(objectsList.contains(new Persona("amaia","2222")) &&
+                objectsList.size() == 3);
+        System.out.println("\t\t"+objectsList.toString());
+        //7 to < 1 2 >
+        System.out.print("\t\t-Adding [ana 3333]: ");
+        objectsList.addAfter(new Persona("ana","3333"),new Persona("pedro","1111"));
+        System.out.println(objectsList.contains(new Persona("ana","3333"))
+                && objectsList.size() == 4);
+        System.out.println("\t\t"+objectsList.toString());
+
+
     }
 
 
--- Gestion_Web_test/src/eus/ehu/ikasle/eda/segundaFase/UnorderedCircularLinkedList.java
+++ Gestion_Web_test/src/eus/ehu/ikasle/eda/segundaFase/UnorderedCircularLinkedList.java
@@ -3,54 +3,70 @@ package eus.ehu.ikasle.eda.segundaFase;
 import java.util.NoSuchElementException;
 
 public class UnorderedCircularLinkedList<T extends Comparable<T>> extends CircularLinkedList<T> implements UnorderedListADT<T> {
-	
-	public void addToFront(T elem) { // O(1)
-	// a�ade un elemento al comienzo
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-		Node<T> elemNode = new Node<T>(elem);
-		if(this.count != 0){
-            elemNode.next = this.last.next;
-            this.last.next = elemNode;
-        }else{
-		    this.last = elemNode;
-		    elemNode.next = last;
-        }
-        this.count++;
-	}
 
-	public void addToRear(T elem) { // O(1)
-	// a�ade un elemento al final 
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+    public void addToFront(T elem) { // O(1)
+        // a�ade un elemento al comienzo
+        // COMPLETAR EL CODIGO Y CALCULAR EL COSTE
         Node<T> elemNode = new Node<T>(elem);
-        if (this.count != 0 ){
+        if (this.count != 0) {
+            elemNode.next = this.last.next;
+            this.last.next = elemNode;
+        } else {
+            this.last = elemNode;
+            elemNode.next = last;
+        }
+        this.count++;
+    }
+
+    public void addToRear(T elem) { // O(1)
+        // a�ade un elemento al final
+        // COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+        Node<T> elemNode = new Node<T>(elem);
+        if (this.count != 0) {
             elemNode.next = this.last.next;
             this.last.next = elemNode;
             this.last = elemNode;
-        }else{
+        } else {
             this.last = elemNode;
             this.last.next = elemNode;
         }
         this.count++;
-	}
+    }
 
-	public void addAfter(T elem, T target) { //O(n) n= numero de elementos
-	// A�ade elem detr�s de otro elemento concreto, target,  que ya se encuentra en la lista
-		// �COMPLETAR OPCIONAL!
+    public void addAfter(T elem, T target) { //O(n) n= numero de elementos
+        // A�ade elem detr�s de otro elemento concreto, target,  que ya se encuentra en la lista
+        // �COMPLETAR OPCIONAL!
         // Target ya viene de la lista o es copia ?
         //Si ya viene
         Node tmp; // anterior al target
-        if (count != 0){
-            while(!(tmp = this.last.next).next.data.equals(target));
-            if (tmp.next.data.equals(target)){
-                Node<T> elemNode  = new Node<>(elem);
+        if (count != 0) {
+            tmp = this.last.next;
+            if (this.last.data.equals(target)) {
+                Node<T> elemNode = new Node<>(elem);
+                elemNode.next = this.last.next;
+                this.last.next = elemNode;
+                this.last = elemNode;
+                this.count++;
+            } else if (this.last.next.data.equals(target)) {
+                Node<T> elemNode = new Node<>(elem);
                 elemNode.next = tmp.next;
                 tmp.next = elemNode;
                 this.count++;
-            }else{
-                throw new NoSuchElementException("Target not found");
+            } else {
+                tmp = tmp.next;
+                while (!tmp.equals(this.last) && !tmp.data.equals(target)) {
+                    tmp = tmp.next;
+                }
+                if (tmp.data.equals(target)) {
+                    Node<T> elemNode = new Node<>(elem);
+                    elemNode.next = tmp.next;
+                    tmp.next = elemNode;
+                    this.count++;
+                } else {
+                    throw new NoSuchElementException("Target not found");
+                }
             }
         }
-        //TODO me da que no hace lo que tiene que hacer
-	}
+    }
 
 }
