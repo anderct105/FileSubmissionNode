3de4b48180ecae3bddb14027404e19b2fa98b71a
Naiara <naiaramaneiro07@gmail.com>
2018-11-12 19:50:50 +0100

    ander guapo

---
+++ practica3/src/eus/ehu/ikasle/eda/Main.java
@@ -0,0 +1,10 @@
+package eus.ehu.ikasle.eda;
+
+public class Main {
+    public static void main(String[] args){
+        Graph g = new Graph();
+        System.out.println("----------Menu----------");
+        System.out.println("1.Prueba estanConcectados()");
+        System.out.println("2.Prueba ");
+    }
+}
