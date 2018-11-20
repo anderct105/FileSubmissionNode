caf98433e953eab1bc9d19262fdd92ebaf4bc4cc
ander <anderct105@gmail.com>
2018-11-20 09:52:22 +0100

    Merge branch 'master' of https://gitlab.com/proyectos_uni/practica-1
    # Conflicts:
    #	Gestion_Web_test/src/eus/ehu/ikasle/eda/Webs.java

--- practica3/src/eus/ehu/ikasle/eda/Main.java
+++ practica3/src/eus/ehu/ikasle/eda/Main.java
@@ -1,5 +1,6 @@
 package eus.ehu.ikasle.eda;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,9 @@ public class Main {
     public static void main(String[] args){
         Graph g = new Graph();
         carga(g);
-        int opcion = 0;
+        pruebaEstanConectados(g);
+        pruebaBackPointer(g);
+        /*int opcion = 0;
         while(opcion != 3) {
             opcion = menu();
             switch (opcion) {
@@ -18,7 +21,7 @@ public class Main {
                     pruebaBackPointer(g);
                     break;
             }
-        }
+        }*/
     }
 
     private static void carga(Graph g){
@@ -41,16 +44,49 @@ public class Main {
     private static void pruebaEstanConectados(Graph g){
         //No estan conectados
         boolean conectados;
+        System.out.println("----------------------------Prueba estanConectados()----------------------------");
         conectados = g.estanConectados("0086k.com","007carteblanche.co.uk");
-        System.out.println("Esta conectado\n la web **0086k.com** cuyo id es 1 a \nla web **007carteblanche.co.uk** cuyo id es 7?" +
-                "la respuesta deberia ser False y es: "+conectados);
+        System.out.println("Prueba1:");
+        System.out.println("Web1: **0086k.com** ID1: 1\nWeb2: **007carteblanche.co.uk** ID2: 7");
+        System.out.println("Respuesta teorica: false");
+        System.out.println("Respuesta practica: "+conectados);
         conectados = g.estanConectados("012design.com","007-taxi.ru");
-        System.out.println("Esta conectado\n la web **012design.com** cuyo id es 2 a \nla web **007-taxi.ru** cuyo id es 5?" +
-                "la respuesta deberia ser False y es: "+conectados);
-        //conectados = g.estanConectados();
+        System.out.println("Prueba2:");
+        System.out.println("Web1: **012design.com** ID1: 2\nWeb2: **007-taxi.ru** ID2: 5");
+        System.out.println("Respuesta teorica: false");
+        System.out.println("Respuesta practica: "+conectados);
+        //estan conectados
+        conectados = g.estanConectados("0086k.com","012design.com");
+        System.out.println("Prueba3:");
+        System.out.println("Web1: **0086k.com** ID1: 1\nWeb2: **012design.com** ID2: 2");
+        System.out.println("Respuesta teorica: true");
+        System.out.println("Respuesta practica: "+conectados);
+        conectados = g.estanConectados("0-3ani.ro","007hebergement.com");
+        System.out.println("Prueba4:");
+        System.out.println("Web1: **0-3ani.ro** ID1: 0\nWeb2: **007hebergement.com** ID2: 6");
+        System.out.println("Respuesta teorica: true");
+        System.out.println("Respuesta practica: "+conectados);
     }
 
     private static void pruebaBackPointer(Graph g){
-
+        System.out.println("----------------------------Prueba backPointer()----------------------------");
+        ArrayList<Integer> camino = new ArrayList<>();
+        System.out.println("Prueba1:");
+        System.out.println("Web1: **0086k.com** ID1: 1\nWeb2: **007carteblanche.co.uk** ID2: 7");
+        System.out.println("El camino deberia ser: []");
+        g.backPointer("0086k.com","007carteblanche.co.uk");
+        System.out.println("Prueba2:");
+        System.out.println("Web1: **012design.com** ID1: 2\nWeb2: **007-taxi.ru** ID2: 5");
+        System.out.println("El camino deberia ser: []");
+        g.backPointer("012design.com","007-taxi.ru");
+        //estan conectados
+        System.out.println("Prueba3:");
+        System.out.println("Web1: **0086k.com** ID1: 1\nWeb2: **012design.com** ID2: 2");
+        System.out.println("Respuesta teorica: true");
+        g.backPointer("0086k.com","012design.com");
+        System.out.println("Prueba4:");
+        System.out.println("Web1: **0-3ani.ro** ID1: 0\nWeb2: **007hebergement.com** ID2: 6");
+        System.out.println("Respuesta teorica: true");
+        g.backPointer("0-3ani.ro","007hebergement.com");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
     }
 }
